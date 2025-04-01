15185dc53812e5907d6f8b80c37282c1d2f45c3e
Amornthep <amornthep.tee@gmail.com>
2025-04-02 01:10:21 +0700

    edit firebaseinit

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArHClbDI5B-BHkeuG-RVWZgHMyMct0v2g",
-  authDomain: "fireblogsyt-4ea2f.firebaseapp.com",
-  projectId: "fireblogsyt-4ea2f",
-  storageBucket: "fireblogsyt-4ea2f.firebasestorage.app",
-  messagingSenderId: "830654839178",
-  appId: "1:830654839178:web:2562e8c3517b0efb74b073",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
